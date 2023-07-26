aebee036bd273ded6db06852e5d5f040cc70b29e
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-07-26 20:56:24 +0200

    update js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
         // EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
         window.onload = function () {
             document.querySelector('.blog-sidebar div:last-child li:nth-of-type(2)').remove();
-        }
+        
             // EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
-            let links = document.querySelectorAll('.stretched-link');
-            for (let i = 0; i < links.length; i++) {
-                links[i].onclick = function(event) {
-                    event.preventDefault();
-                    event.target.closest('.col-md-6').remove();
-                };
-            }
+            const links = document.querySelectorAll(".jumbotron a,.card-text + a");
+            for (const link of links){
+                link.addEventListener("click",function(event){
+                    event.target.parentNode.remove()})
+                }
+            
             
             // EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
             
@@ -20,6 +19,7 @@
                     alert(event.target.innerText);
                 });
             }
+        }
 
 
         
